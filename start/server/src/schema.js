968753059355b1fa66d6f364602d7e92a1c43619
Denis Neverov <denis.neverov@gmail.com>
2020-04-12 04:32:01 +0300

    Add Resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -22,9 +22,13 @@ const typeDefs = gql`
     trips: [Launch]!
   }
 
+  # type Mission {
+  #   name: String
+  #   missionPatch(size: PatchSize): String
+  # }
   type Mission {
-    name: String
-    missionPatch(size: PatchSize): String
+    name: String # ... with rest of schema
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
@@ -33,9 +37,29 @@ const typeDefs = gql`
   }
 
   type Query {
-    launches: [Launch]!
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   # Queries enable clients to fetch data, but not to modify data.
